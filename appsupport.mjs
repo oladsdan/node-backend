e96ad007a1d9c3ad4547e0885d20da47ed7182e2
oladsdan <104092480+oladsdan@users.noreply.github.com>
2023-03-27 18:53:17 +0100

    complete changes form common js to use es6 app.mjs

--- appsupport.mjs
+++ appsupport.mjs
@@ -14,11 +14,60 @@ export function normalizePort(val) {
     return false;
 }
 
-
+// this prints a user-friendly message saying where the server is listening for 
 export function onListening() {
     const addr = server.address();
     const bind = typeof addr === 'string'
         ? 'pipe' + addr
         : 'port' + addr.port;
     console.log(`Listening on ${bind}`);
+}
+
+export function handle404(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+
+export function basicErrorHandler(err, req, res, next) {
+    //Defer to built-in error handler if headersSent
+    //see: http://expressjs.come/en/guide/error-handling.html
+    if (res.headersSent) {
+        return next(err)
+    }
+    //sdt locals, only providing errror in development
+    res.locals.message = err.message;
+    res.local.Error=req.app.get('env') === 'development'? err : {};
+
+    //render the error page
+    res.status(err.status || 500);
+    res.render('error');
+}
+
+
+/**
+ * Event listerner for Http Server "error" event
+ */
+
+export function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe' + port
+        : 'Port' + port;
+
+    switch (error.code) {
+        case 'EACESS':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
